a56e7320a2c4f33590313eca7ac9a767c44245df
Patrick Huang <pat.b.huang@gmail.com>
2021-03-20 21:28:39 -0700

    updated drawing to screen buffer

--- main.cpp
+++ main.cpp
@@ -4,8 +4,11 @@
 #include "vec3.h" 
 #include "ray.h"
 
+#define WIDTH 120
+#define HEIGHT 15
 
-int main()
+/*
+void tests()
 {
 	vec3 test(0.0f, 0.1f, 0.02f);
 	vec3 test2(0.1f, 0.2f, 0.03f);
@@ -13,5 +16,28 @@ int main()
 	vec3 result = test + test2;
 	ray r(test, test2);
 	printf("testx : %F", r.direction.x );
+	
+}
+*/
+
+int main()
+{
+	int screenSize = WIDTH * HEIGHT;
+	char* screen = new char[screenSize];
+	for (int i = 0; i < screenSize; i++) {
+		screen[i] = 'a';
+	}
+	HANDLE console = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
+	SetConsoleActiveScreenBuffer(console);
+	DWORD bytesWritten = 0;
+	
+	while (1) {
+		screen[3] = 'a';
+		screen[4] = 'b';
+		screen[screenSize - 1] = '\0';
+		WriteConsoleOutputCharacter(console, screen, screenSize, { 0,0 }, &bytesWritten);
+	}
+
+	
 	return 0;
 }
--- ray.cpp
+++ ray.cpp
@@ -1,6 +1,8 @@
 #include "ray.h"
 #include "vec3.h"
 
+ray::ray() {}
+
 ray::ray (vec3 point, vec3 direction)
 {
 	this->point = point;
